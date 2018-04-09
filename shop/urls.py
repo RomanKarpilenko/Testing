469dfbba0849d280adf232aa3a38a8ac6820cb7b
Loocor <loocor1@gmail.com>
2018-04-10 00:18:52 +0300

    123

--- shop/urls.py
+++ shop/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^shop/$', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
+from .forms import SubscriberForm
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    form = SubscriberForm(request.POST or None)
+    if request.method == "POST" and form.is_valid():
+        new_form = form.save()
+    return render(request, 'shop/index.html', locals())
+
+def detail(request, question_id):
+    return HttpResponse("You're looking %s at question." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at %s the results of question ."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
